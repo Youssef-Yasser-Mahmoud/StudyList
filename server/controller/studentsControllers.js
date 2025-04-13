d6bf1dea849516eee7e20eb8690a15306709d93f
Youssef <youssefyaser2020@gmail.com>
2025-04-13 09:00:54 +0200

    Create getStudent, studentForm and pushStudent

--- server/controller/studentsControllers.js
+++ server/controller/studentsControllers.js
@@ -6,4 +6,19 @@ const getAllStudents = async (req, res) => {
   res.render('students', { students });
 };
 
-module.exports = { getAllStudents };
+const studentForm = (req, res) => {
+  res.render('addStudent');
+};
+
+const pushStudent = async (req, res) => {
+  const studentObj = req.body;
+  await Model.students.insertOne(studentObj);
+  res.redirect('students');
+};
+
+const getStudent = (req, res) => {
+  console.log(req.params.id)
+  res.send('hi')
+};
+
+module.exports = { getAllStudents, studentForm, pushStudent, getStudent };
--- server/routes/studentsRoutes.js
+++ server/routes/studentsRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const studentsControllers = require('../controller/studentsControllers');
 
 router.get('/', studentsControllers.getAllStudents);
+router.get('/:id', studentsControllers.getStudent);
+router.get('/add', studentsControllers.studentForm);
+router.post('/', studentsControllers.pushStudent)
 
 module.exports = router;
